43b673116023b83e210fd55980765ebaf36f1335
felixtaljaard1 <felix.taljaard@informationtechconsultants.co.uk>
2022-09-14 14:34:22 +0100

    please

--- app/src/main/java/com/example/pushnotz/MainActivity.kt
+++ app/src/main/java/com/example/pushnotz/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // this is so annoying
+        // why is this so annoying
     }
 }
